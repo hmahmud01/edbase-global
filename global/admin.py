673f03987fe98c0fa3b6cb03ddbd5961e8d0004c
hmahmud01 <hmahmud01@gmail.com>
2022-10-09 23:29:02 +0600

    v2 check first

--- global/admin.py
+++ global/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register([Qualification, Teacher, Student, PersonalInfo, Country, University, Directory, Content, DirectoryIndex])
